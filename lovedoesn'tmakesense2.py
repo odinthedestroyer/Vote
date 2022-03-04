1583b3c54d418fd9953f2c2a198509d164bd6053
odinthedestroyer <85331179+odinthedestroyer@users.noreply.github.com>
2022-03-04 21:29:05 +0400

    Add files via upload

---
+++ lovedoesn'tmakesense2.py
@@ -0,0 +1,43 @@
+
+
+
+
+import time
+import os
+os.system('cls')
+
+os.system('color')
+print('\x1b[6;37;40m' + 'Parting is all we know of heaven, and all we need of hell.' + '\x1b[0m')
+time.sleep(5)
+if os.name in ('nt', 'dos'):
+ os.system('cls')
+else:
+ os.system('clear')
+
+os.system('color')
+
+
+
+print('\x1b[7;30;41m' + 'What you think? Which one of them want to go to hell?' + '\x1b[0m')
+time.sleep(5)
+
+
+print('Valrise:',end=" ")
+print("78%",)
+print("█ █ █ █ █ █ █ █ █ █", end=" "), time.sleep(3)
+print("█ █ █ █ █", end=" "), time.sleep(2)
+print("█ █ █")
+
+time.sleep(3)
+
+print('Italy: 12%',)
+print("█ █ █", end=" ")
+time.sleep(5)
+print("█ █ █")
+
+
+time.sleep(10)
+os.system('cls')
+
+print('\x1b[7;30;41m' + 'Dont be shy, talk to me dear Valrise.' + '\x1b[0m')
+time.sleep(60)
